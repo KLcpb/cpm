579e5669a8691637d31a641cbc364c083c384ee0
olymp <3123@313.com>
2023-09-14 09:45:45 +0300

    v1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("test")
-print("helloworld")
+import math
+a = int(input())
+b = int(input())
+
+
+for i in range(a,b+1):
+    div = []
+    cnt = 0
+    for d in range(1,math.ceil(i**0.5) + 2):
+        if i % d ==0:
+            cnt+=1
+            div.append(d)
+
+    div.append(i)
+
+    if len(div) == 4:
+        print(i)
