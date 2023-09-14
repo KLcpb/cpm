d80baf7d635f8ce3e825c6b01da7976d568f9a02
olymp <3123@313.com>
2023-09-14 10:04:51 +0300

    final version3

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import math
-a = int(input())
-b = int(input())
 
+try:
+    a = int(input())
+    b = int(input())
+except:
+    print("wrong input format:((((((((")
 
-for i in range(a,b+1):
+for i in range(a-1,b+1):
+    if i == 0:
+        continue
     div = []
     cnt = 0
     for d in range(1,math.ceil(i**0.5) + 2):
         if i % d ==0:
             cnt+=1
-            div.append(d)
+            if i % (i//d) == 0 and i//d not in div:
+                div.append(i//d)
 
-    div.append(i)
+            if d not in div:
+                div.append(d)
+    #print(div)
     if len(div) == 4:
+        div.sort()
         div.reverse()
         print(i,"divisors: ",div[0],div[1],div[2],div[3])
