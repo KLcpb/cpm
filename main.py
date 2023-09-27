e47b41a63cd2c14b7f0a77fb3870dc7aa07baa67
KLcpb <138727426+KLcpb@users.noreply.github.com>
2023-09-27 22:12:54 +0300

    Permutation

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import math
+s = [1,2,3]
+def p(l,n):
+	if n < 2:
+		print(l)
+	else:
+		g = list(range(n))
+		g.reverse()
+		for i in g:
+			l[i],l[n-1] = l[n-1],l[i]
+			p(l,n-1)
+			l[i],l[n-1] = l[n-1],l[i]
 
-try:
-    a = int(input())
-    b = int(input())
-except:
-    print("wrong input format:((((((((")
 
-for i in range(a-1,b+1):
-    if i == 0:
-        continue
-    div = []
-    cnt = 0
-    for d in range(1,math.ceil(i**0.5) + 2):
-        if i % d ==0:
-            cnt+=1
-            if i % (i//d) == 0 and i//d not in div:
-                div.append(i//d)
 
-            if d not in div:
-                div.append(d)
-    #print(div)
-    if len(div) == 4:
-        div.sort()
-        div.reverse()
-        print(i,"divisors: ",div[0],div[1],div[2],div[3])
+
+p(s,3)
+
